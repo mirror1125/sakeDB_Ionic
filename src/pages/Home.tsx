35d03812265d8e41c33d8be3db5d2e164a090523
D.Kagami <mirrorseek@gmail.com>
2020-12-27 16:31:35 +0900

    新規作成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,38 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React from "react";
+import { add } from "ionicons/icons";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>酒データベース</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">酒データベース</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton href="/register">
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
